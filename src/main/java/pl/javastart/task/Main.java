d9fb12f4093603fafdeb2965c269e466584218e8
Bartek Krawczyk <bartoszkrawczykk@gmail.com>
2023-03-07 19:42:44 +0100

    rozwiązanie 1 (nie bardzo wiem jak obliczyć sumę zapisanych w tablicy liczb i ją wyświetlić)

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,24 @@
 package pl.javastart.task;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj rozmiar tablicy:");
+        int tmp = sc.nextInt();
+        System.out.println("Wprowadź 5 liczb:");
 
+        int [] numbers = new int[tmp];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        for (int suma : numbers) {
+            System.out.println("Suma kwadratów wprowadzonych liczb to " + suma);
+        }
+        sc.close();
     }
 }
